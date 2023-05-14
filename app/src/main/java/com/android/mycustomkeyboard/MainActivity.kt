321300a7c5222d17ab2cd28901803a58ddf8106d
Vemir <vemir.amirkhanyan@gmail.com>
2023-05-15 02:46:47 +0400

    Created enable keyboard and choose keyboard methods and added hilt dependency.

--- app/src/main/java/com/android/mycustomkeyboard/MainActivity.kt
+++ app/src/main/java/com/android/mycustomkeyboard/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.android.mycustomkeyboard
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import com.android.mycustomkeyboard.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+	private var binding: ActivityMainBinding? = null
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
+		binding = ActivityMainBinding.inflate(layoutInflater).also { binding ->
+			setContentView(binding.root)
+			with(binding) {
+				activityMainSettingsButton.setOnClickListener {
+					enableKeyboard()
+				}
+				activityMainKeyboardButton.setOnClickListener {
+					chooseKeyboard()
+				}
+			}
+		}
+
+	}
+
+	private fun chooseKeyboard() {
+		val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+		inputMethodManager.showInputMethodPicker()
+	}
+
+	private fun enableKeyboard() {
+		val intent = Intent(Settings.ACTION_INPUT_METHOD_SETTINGS)
+		startActivity(intent)
 	}
 }
---
+++ app/src/main/java/com/android/mycustomkeyboard/MyCustomKeyboardApp.kt
@@ -0,0 +1,8 @@
+package com.android.mycustomkeyboard
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyCustomKeyboardApp : Application() {
+}
